d3e701cac6f1762a6aca81a0a33b5a907a23905e
yuji96 <y.yama683@gmail.com>
2022-12-19 11:32:48 +0900

    update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,30 @@
+#!/bin/bash
+
+# tail -f /dev/null
+
+# TODO: とりあえず今は直で指定
+MODEL_NAME=sample
+
 cd /app/src
-git clone https://github.com/tesseract-ocr/tesstrain.git
-tail -f /dev/null
+if [ ! -d tesstrain ]; then
+  git clone --depth=1 https://github.com/tesseract-ocr/tesstrain.git
+fi
+cd tesstrain
+
+# TODO: use symlink, remove previous ground-truth
+rm -rf /app/src/tesstrain/data/$MODEL_NAME-ground-truth/
+mkdir -p /app/src/tesstrain/data/$MODEL_NAME-ground-truth
+cp -s /app/data/ground-truth/* \
+      /app/src/tesstrain/data/$MODEL_NAME-ground-truth/
+
+echo $(cat /app/args.txt | sed "s/#.*//" | xargs)
+
+# FIXME
+ln -s /usr/local/share/tessdata/eng_best.traineddata /usr/local/share/tessdata/eng.traineddata
+
+# TODO: use DATA_DIR
+if [ ! -d data/langdata ]; then
+  make tesseract-langdata
+fi
+
+cat /app/args.txt | sed "s/#.*//" | xargs make training
